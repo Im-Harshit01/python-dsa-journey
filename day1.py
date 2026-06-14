0651224bac0d428b11b8fad275e6c69be37c8479
Harshit Singh <harshitsingh6405@gmail.com>
2026-06-15 03:05:47 +0530

    Create day1.py

---
+++ day1.py
@@ -0,0 +1,9 @@
+name = "Harshit Singh"
+age = 19
+profession = "Student"
+hobbies = ["traveling", "Coding", "Gaming"]
+
+print("Hello", name)
+print(f"you're {age} years old")
+print("your profession is that you're a", profession)
+print("Hobbies:", ", ".join(hobbies))
