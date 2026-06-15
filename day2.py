d047ce035e4ec2cb1b9b3ec187ae007194564f97
Harshit Singh <harshitsingh6405@gmail.com>
2026-06-16 01:16:45 +0530

    Create day2.py

---
+++ day2.py
@@ -0,0 +1,23 @@
+num_1 = float(input("Enter the first number: "))
+abbrviation = input("Enter operation (+, -, *, /): ")
+num_2 = float(input("Enter the second number: "))
+
+if abbrviation not in ["+", "-", "*", "/"]:
+    print("wrong abbreviation input")
+    exit()
+
+if abbrviation == "+":
+    result = num_1 + num_2
+    print(f"The result of {num_1} + {num_2} is: {result:.3f}")
+elif abbrviation == "-":
+    result = num_1 - num_2
+    print(f"The result of {num_1} - {num_2} is: {result:.3f}")
+elif abbrviation == "*":
+    result = num_1 * num_2
+    print(f"The result of {num_1} * {num_2} is: {result:.3f}")
+elif abbrviation == "/":
+    if num_2 == 0:
+        print("Error: Cannot divide by zero")
+    else:
+        result = num_1 / num_2
+        print(f"The result of {num_1} / {num_2} is: {result:.3f}")
